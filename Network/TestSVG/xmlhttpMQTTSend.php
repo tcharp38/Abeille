9d206723228c1afded50d14fe58fd42be975578e
Tcharp38 <tcharp38@laposte.net>
2022-01-21 13:29:58 +0100

    Post resync fixes

--- Network/TestSVG/xmlhttpMQTTSend.php
+++ Network/TestSVG/xmlhttpMQTTSend.php
@@ -40,15 +40,7 @@
         echo "debug","(fichier xmlhttpMQQTSend) could not add message to queue";
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     if (msg_send($queueKeyXmlToCmd, 1, $msg, true, false)) {
-=======
-    if (msg_send( $queueKeyXmlToCmd, 1, $msg, true, false)) {
->>>>>>> c53a62bf (msgAbeille => msg + format simplified)
-=======
-    if (msg_send($queueKeyXmlToCmd, 1, $msg, true, false)) {
->>>>>>> 90af900d (Queues again)
         echo "(fichier xmlhttpMQQTSend) added to queue: ".json_encode($msg);
         // print_r(msg_stat_queue($queue));
     }
