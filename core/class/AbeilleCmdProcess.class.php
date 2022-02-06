c9dfb2423facd09751caf6cfc9a637a618c9689b
Tcharp38 <tcharp38@laposte.net>
2022-02-06 15:27:41 +0100

    Some resync with Abeille

--- core/class/AbeilleCmdProcess.class.php
+++ core/class/AbeilleCmdProcess.class.php
@@ -3751,7 +3751,6 @@
                 }
 
                 // ZCL cluster 0008/Level control specific: (received) commands
-                // setLevel on one object
                 // Mandatory params: addr, EP, Level (in dec, %), duration (dec)
                 // Optional params: duration (default=0001)
                 else if ($cmdName == 'setLevel') {
@@ -4312,26 +4311,26 @@
                     $clustId    = 'EF00';
                     $secMode    = "02";
                     $radius     = "1E";
-        
+
                     /* ZCL header */
                     $fcf        = "11"; // Frame Control Field
                     $sqn        = "23";
                     $cmdId      = "00";
-        
+
                     $status         = "00";
                     $counterTuya    = "33"; // Set to 1, in traces increasse all the time. Not sure if mandatory to increase.
                     $cmdTuya        = $Command['cmd'];
                     if ($cmdTuya==GotoLevel) $type = DataType_VALUE; else $type = DataType_ENUM;
                     $function         = "00";
                     if ($type==DataType_VALUE) $len = "04"; else $len="01";
-                    if ($type==DataType_VALUE) $param = sprintf("%08s", dechex($Command['param'])); else $param = $Command['param'];  // Up=00, Stop: 01, Down: 02 
-        
+                    if ($type==DataType_VALUE) $param = sprintf("%08s", dechex($Command['param'])); else $param = $Command['param'];  // Up=00, Stop: 01, Down: 02
+
                     $data2 = $fcf.$sqn.$cmdId.$status.$counterTuya.$cmdTuya.$type.$function.$len.$param;
                     $dataLen2 = sprintf("%02s", dechex(strlen($data2) / 2));
-        
+
                     $data1 = $addrMode.$addr.$srcEp.$dstEp.$clustId.$profId.$secMode.$radius.$dataLen2;
                     $data = $data1.$data2;
-        
+
                     $this->addCmdToQueue2(priorityUserCmd, $dest, $cmd, $data, $addr, $addrMode, AckAPS);
                     return;
                 }
